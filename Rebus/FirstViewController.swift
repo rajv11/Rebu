0d29b9c10dfd8c4a0b1b85791c997b69a9c02ade
Jennaikode,Vamshi Raj <s531370@ch140027.nwmissouri.edu>
2018-10-01 12:02:59 -0500

    Initial Commit

---
+++ Rebus/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Rebus
+//
+//  Created by Jennaikode,Vamshi Raj on 10/1/18.
+//  Copyright © 2018 Jennaikode,Vamshi Raj. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Rebus/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Rebus
+//
+//  Created by Jennaikode,Vamshi Raj on 10/1/18.
+//  Copyright © 2018 Jennaikode,Vamshi Raj. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
