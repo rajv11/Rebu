7b2b9658c0715886954dd1d4a392523228a132a6
vamshi raj <student@Adityas-MacBook-Air.local>
2018-10-06 14:21:41 -0500

    Created InChat view and navBar to chat screen

---
+++ Rebus/InChatViewController.swift
@@ -0,0 +1,31 @@
+//
+//  InChatViewController.swift
+//  Rebus
+//
+//  Created by student on 10/6/18.
+//  Copyright © 2018 Jennaikode,Vamshi Raj. All rights reserved.
+//
+
+import UIKit
+
+class InChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Allen"
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
