2ee7490171f91fe8355bacaa68522f16c3fc54a1
SANTOSH SEKHAR M B <santosh@santoshs-mbp.nwmissouri.edu>
2018-10-21 17:58:54 -0500

    added choose ride controller

---
+++ Rebus/RideSelectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RideSelectionViewController.swift
+//  Rebus
+//
+//  Created by SANTOSH SEKHAR M B on 10/21/18.
+//  Copyright © 2018 Jennaikode,Vamshi Raj. All rights reserved.
+//
+
+import UIKit
+
+class RideSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
