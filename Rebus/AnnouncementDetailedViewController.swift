724d14c4975c364f6abc4dc0af5aeb48dbd3cfb5
bunty0211 <s531366@nwmissouri.edu>
2018-10-22 22:06:58 -0500

    created a view controller for the announcement table view controller

---
+++ Rebus/AnnouncementDetailedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AnnouncementDetailedViewController.swift
+//  Rebus
+//
+//  Created by Student on 10/22/18.
+//  Copyright © 2018 Jennaikode,Vamshi Raj. All rights reserved.
+//
+
+import UIKit
+
+class AnnouncementDetailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
