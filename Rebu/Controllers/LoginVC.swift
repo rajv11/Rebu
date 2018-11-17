c2c90003385b1a1136f95d251ad44e4218d26649
Vamshi Raj <student@nw46143.local>
2018-11-16 18:46:29 -0600

    fixed some login issues

--- Rebu/Controllers/LoginVC.swift
+++ Rebu/Controllers/LoginVC.swift
@@ -12,6 +12,13 @@ class LoginVC: UIViewController {
     @IBOutlet weak var userNameTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     @IBOutlet weak var rememberMESwitch: UISwitch!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    
     func display(title:String, msg:String) {
         let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
@@ -20,16 +27,16 @@ class LoginVC: UIViewController {
     }
     @IBAction func loginBtn(_ sender: Any) {
         Backendless.sharedInstance().userService.login(userNameTF.text!,
-                                                       password: passwordTF.text!,
-                                                       response: { user in
-                                                        if user != nil {
-                                                            if self.rememberMESwitch.isOn {
-                                                                Backendless.sharedInstance()?.userService.setStayLoggedIn(true)
-                                                            } else {
-                                                                Backendless.sharedInstance()?.userService.setStayLoggedIn(false)
-                                                            }
-                                                            self.performSegue(withIdentifier: "login", sender: user)
-                                                        }
+                                                    password:passwordTF.text!,
+            response: { user in
+            if user != nil {
+                    if self.rememberMESwitch.isOn {
+                        Backendless.sharedInstance()?.userService.setStayLoggedIn(true)
+                    } else {
+                        Backendless.sharedInstance()?.userService.setStayLoggedIn(false)
+                    }
+                    self.performSegue(withIdentifier: "login", sender: user)
+                }
                                                         
         },error: { fault in
             self.display(title: "Login Failed", msg: (fault?.message!)!)
@@ -37,11 +44,6 @@ class LoginVC: UIViewController {
         })
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
 
     
