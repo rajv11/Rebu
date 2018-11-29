25ca1ac7854117d6cc680f9c1e64b588f383c6dd
bharadwaj0211 <S531366@nwmissouri.edu>
2018-11-28 22:15:33 -0600

    Displayed a Take A Ride Announcements details  and Give a Ride Announcement details

--- Rebu/Controllers/AnnouncementTableViewController.swift
+++ Rebu/Controllers/AnnouncementTableViewController.swift
@@ -72,14 +72,20 @@ class AnnouncementTableViewController: UITableViewController {
         //let sourceViewController = unwindSegue.source
         // Use data from the view controller which initiated the unwind segue
     }
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "TakeARideDetail" {
+            if announcmentData.rideSelect == "Take" {
+                let takeARideVC = segue.destination as! TakeARideDetailsViewController
+                takeARideVC.announcementGive = announcmentData.anouncementGiveRide[tableView.indexPathForSelectedRow!.row]
+            } else {
+                let takeARideVC = segue.destination as! TakeARideDetailsViewController
+                takeARideVC.announcementTake = announcmentData.anouncementTakeRide[tableView.indexPathForSelectedRow!.row]
+            }
+        }
     }
-    */
 
 }
---
+++ Rebu/Controllers/TakeARideDetailsViewController.swift
@@ -0,0 +1,57 @@
+//
+//  TakeARideDetailsViewController.swift
+//  Rebu
+//
+//  Created by student on 11/28/18.
+//  Copyright © 2018 Jennaikode,Vamshi Raj. All rights reserved.
+//
+
+import UIKit
+
+class TakeARideDetailsViewController: UIViewController {
+    
+    var announcementGive:GiveRideAnouncement!
+    var announcementTake:TakeRideAnouncement!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var nameLBL: UILabel!
+    
+    @IBOutlet weak var phoneNumberLBL: UILabel!
+    
+    @IBOutlet weak var informLBL: UILabel!
+    
+    @IBOutlet weak var startLBL: UILabel!
+    
+    @IBOutlet weak var endLBL: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if announcementGive == nil {
+            nameLBL.text = announcementTake.name
+            phoneNumberLBL.text = announcementTake.contact
+            informLBL.text = announcementTake.extraInfo
+            startLBL.text = announcementTake.startPoint
+            endLBL.text = announcementTake.endPoint
+        } else {
+            nameLBL.text = announcementGive.name
+            phoneNumberLBL.text = announcementGive.contact
+            informLBL.text = announcementGive.extraInfo
+            startLBL.text = announcementGive.startPoint
+            endLBL.text = announcementGive.endPoint
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
